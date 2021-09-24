037a0e6da6a130784a90619dde3aed0c315099a2
MeshAndrey <35770825+MeshAndrey@users.noreply.github.com>
2021-09-24 12:04:11 +0300

    Small optimization fix
    Program is smaller for 8 bytes

--- TIM2_pwm/src/main.c
+++ TIM2_pwm/src/main.c
@@ -7,20 +7,20 @@ inline void init_tim2(void)
 {
     CLK->PCKENR1 |= CLK_PCKENR1_TIM2; // Enable TIM2
 
-    TIM2->CR1 |= TIM2_CR1_ARPE; // 
+    TIM2->CR1 = TIM2_CR1_ARPE; // Enable Auto-Reload Preload // reset value = 0
 
-    TIM2->PSCR = 1; // 
+    TIM2->PSCR = 1; // Set Prescaler Value (pow (2, 1) or (2 ^ 1))
 
-    TIM2->ARRH  = 0x03; // high bits of 1000d in auto-reload register
-    TIM2->ARRL  = 0xE8; // low  bits of 1000d in auto-reload register
+    TIM2->ARRH = BYTE_1(1000); // high bits of 1000d in auto-reload register
+    TIM2->ARRL = BYTE_0(1000); // low  bits of 1000d in auto-reload register
 
-    TIM2->CCMR1 |= (1 << 6) | (1 << 5) //| (1 << 4) // PWM mode 1
-	                    | (1 << 3); // Output compare 1 Preload enable
+    TIM2->CCMR1 = (1 << 6) | (1 << 5)  //| (1 << 4) // PWM mode 1
+	                   | (1 << 3); // Output compare 1 Preload enable
 
-    TIM2->CCR1H = 0x00;  // high bits of 500d in Capture/compare register
-    TIM2->CCR1L = 0x2F;  // low  bits of 500d in Capture/compare register
+    //TIM2->CCR1H = 0x00;  // high bits of 500d in Capture/compare register
+    TIM2->CCR1L = BYTE_0(500);  // low  bits of 500d in Capture/compare register
 
-    TIM2->CCER1 |= 1;
+    TIM2->CCER1 = TIM2_CCER1_CC1E; // Set capture/compare output enable
 
     TIM2->CR1 |= TIM2_CR1_CEN; // Counter enabled
 }
