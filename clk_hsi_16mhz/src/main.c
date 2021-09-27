5f20fc8f871dc6da8965adced783fda0efe4591a
MeshAndrey <35770825+MeshAndrey@users.noreply.github.com>
2021-09-27 14:19:51 +0300

    Add inline to functions

--- clk_hsi_16mhz/src/main.c
+++ clk_hsi_16mhz/src/main.c
@@ -5,19 +5,19 @@
 #include <stdint.h>
 #include <stm8s.h>
 
-void delay(uint32_t time)
+inline void delay(uint32_t time)
 {
     while(time--);
 }
 
-void init_gpio(void)
+inline void init_gpio(void)
 {
     SetBit(GPIOB->DDR, 5);    /* set direction as output */
     SetBit(GPIOB->ODR, 5);    /* set output 1            */
     SetBit(GPIOB->CR1, 5);    /* init as push-pull       */
 }
 
-void init_clk_hsi(void) /* set clk to 16 MHz */
+inline void init_clk_hsi(void) /* set clk to 16 MHz */
 {
     CLK->CKDIVR &= ~(CLK_CKDIVR_HSIDIV); /* set HSIDIV bits to 0 */
     CLK->CKDIVR &= ~(CLK_CKDIVR_CPUDIV); /* set CPUDIV bits to 0 */
